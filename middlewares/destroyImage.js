6f45aba72fe1ad39ab93904f7a4cb07b25119a5f
Thant Ko Zaw <thantmkou10@gmail.com>
2022-12-27 18:52:33 +0630

    add a line

--- middlewares/destroyImage.js
+++ middlewares/destroyImage.js
@@ -4,6 +4,7 @@ module.exports = (public_id) => {
   return new Promise((resolve, reject) => {
     cloudinary.uploader.destroy(public_id, (error, result) => {
       if (error) return reject(error);
+
       resolve(result);
     });
   });
